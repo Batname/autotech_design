bcd56d065e70083ec12304d0a44643f2cd5bd931
Batname <dadubinin@gmail.com>
2014-08-10 12:57:57 +0300

    Element position in Header

--- skin/frontend/default/autotech/js/jm.script.js
+++ skin/frontend/default/autotech/js/jm.script.js
@@ -3,14 +3,14 @@ if (window.jQuery && jQuery.noConflict && (typeof $('body') == 'object')) {
 /**
 	 * ja tabs plugin
 	 */
-	(function($) { 
+	(function($) {
 				$.fn.jaContentTabs = function (_options){
-					return this.each( function() {		
+					return this.each( function() {
 						var container = $( this );
 						new $.jaContentTabs().setup( this, container );
 					} );
 				}
-				 $.jaContentTabs = function() { 
+				 $.jaContentTabs = function() {
 					var self = this;
 					this.lastTab = null;
 					this.nextTab=null;
@@ -18,23 +18,23 @@ if (window.jQuery && jQuery.noConflict && (typeof $('body') == 'object')) {
 						var contentTabs = o.children( 'div.ja-tab-content' );
 						var nav = o.children( 'ul.ja-tab-navigator' );
 						contentTabs.children('div').hide();
-						nav.children('li:first').addClass('first').addClass( 'active' );	
+						nav.children('li:first').addClass('first').addClass( 'active' );
 						contentTabs.children('div:first').show();
-						
+
 						nav.children('li').children('a').click( function() {
-							self.lastTab = 	nav.children('li.active').children('a').attr('href');										
-							nav.children('li.active').removeClass('active')											
+							self.lastTab = 	nav.children('li.active').children('a').attr('href');
+							nav.children('li.active').removeClass('active')
 							$(this).parent().addClass('active');
-							var currentTab = $(this).attr('href'); 
-							self.tabActive( contentTabs, currentTab );		
+							var currentTab = $(this).attr('href');
+							self.tabActive( contentTabs, currentTab );
 							return false;
-						});	
+						});
 					};
 					this.tabActive=function( contentTabs,  currentTab ){
 						if(  this.lastTab != currentTab ){
-							contentTabs.children( this.lastTab ).hide();	
+							contentTabs.children( this.lastTab ).hide();
 						}
-			
+
 						contentTabs.children( currentTab ).show();
 					};
 				};
@@ -96,13 +96,13 @@ jQuery.fn.addSpan = function() {
 	});
 };
 
-document.observe("dom:loaded", function() {   
-	
+document.observe("dom:loaded", function() {
+
 	backtotop = jQuery('<a href="#Top" class="btn-btt" title="Back to Top" id="button-btt" style="display: inline;"><i class="fa fa-caret-up"></i>Top</a>');
 	backtotop.appendTo('body');
 	// hide #back-top first
 	backtotop.hide();
-	
+
 	// fade in #back-top
 	jQuery(function () {
 		jQuery(window).scroll(function () {
@@ -111,37 +111,37 @@ document.observe("dom:loaded", function() {
 			} else {
 				backtotop.fadeOut();
 			}
-			
+
 			if(jQuery(this).scrollTop() == 0){
 				jQuery("#jm-head").removeClass("headscroll");
-				jQuery("#jm-quickaccess").before(jQuery("#jm-mycart"));
-				jQuery("#jm-search").css("margin-right",187);
+				//jQuery("#jm-quickaccess").before(jQuery("#jm-mycart"));
+				//jQuery("#jm-search").css("margin-right",187);
 			}else if(!jQuery("#jm-head").hasClass("headscroll")){
 				if(jQuery(window).width() > 984 ){
-                    jQuery("#jm-search").before(jQuery("#jm-mycart"));
-                    jQuery("#jm-search").css("margin-right",0);
-				} 
+                  //  jQuery("#jm-search").before(jQuery("#jm-mycart"));
+                    //jQuery("#jm-search").css("margin-right",0);
+				}
                 jQuery("#jm-head").addClass("headscroll");
 			}
-			
+
 		});
         jQuery(window).resize(function(){
 				if(jQuery(window).width() < 984){
-					jQuery("#jm-quickaccess").before(jQuery("#jm-mycart"));
+					//jQuery("#jm-quickaccess").before(jQuery("#jm-mycart"));
 				}else{
-					jQuery("#jm-search").before(jQuery("#jm-mycart"));
-                    jQuery("#jm-search").css("margin-right",0);
+					//jQuery("#jm-search").before(jQuery("#jm-mycart"));
+                    //jQuery("#jm-search").css("margin-right",0);
 				}
 		    });
 	    jQuery(document).ready(function(){
 
 	    	if(jQuery(window).width() > 984 && jQuery(this).scrollTop() > 0 ){
 
-                jQuery("#jm-search").before(jQuery("#jm-mycart"));
-                jQuery("#jm-search").css("margin-right",0);
+               // jQuery("#jm-search").before(jQuery("#jm-mycart"));
+                //jQuery("#jm-search").css("margin-right",0);
 	    	}
 	    	jQuery("#jm-mycart").css("display","block");
-	    	
+
 	    });
 		// scroll body to 0px on click
 		backtotop.click(function () {
@@ -151,14 +151,14 @@ document.observe("dom:loaded", function() {
 			return false;
 		});
 	});
-	
-	
-	// initially hide all containers for tab content $$('div.tabcontent').invoke('hide'); 
+
+
+	// initially hide all containers for tab content $$('div.tabcontent').invoke('hide');
 	menuFistLastItem();
 	navMouseHover();
 	//jQuery ('.page-title h1').addSpan();
 	jQuery(".block-content .currently ol").children().last().addClass("last");
-}); 
+});
 
 //Add hover event for li - hack for IE6
 function navMouseHover () {
