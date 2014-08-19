4d075b7f2e5417e6bf55d7a1397fc99e7841b99d
Batname <dadubinin@gmail.com>
2014-08-19 16:48:15 +0300

    Catalog-list

---
+++ tests/sort_array.php
@@ -0,0 +1,25 @@
+<?php
+$people = array(
+    array("name"=>"Bob","age"=>8,"colour"=>"red"),
+    array("name"=>"Greg","age"=>12,"colour"=>"blue"),
+    array("name"=>"Andy","age"=>52,"colour"=>"purple"));
+
+var_dump($people);
+
+$sortArray = array();
+
+foreach($people as $person){
+    foreach($person as $key=>$value){
+        if(!isset($sortArray[$key])){
+            $sortArray[$key] = array();
+        }
+        $sortArray[$key][] = $value;
+    }
+}
+
+$orderby = "age"; //change this to whatever key you want from the array
+
+array_multisort($sortArray[$orderby],SORT_ASC,$people);
+
+var_dump($people);
+?> 
