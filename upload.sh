c40ac1d83f0f98f90e86ffac04068b28f4f0da9f
Batname <dadubinin@gmail.com>
2014-12-21 11:15:33 +0200

    Searchanise_Search_Indexer

--- upload.sh
+++ upload.sh
@@ -10,7 +10,9 @@ then
 iconv -f WINDOWS-1251 -t UTF-8 /var/www/autotech/autotech.ua/var/import/import-1c.csv > /var/www/autotech/autotech.ua/var/import/import-1c-utf8.csv
 /usr/bin/php /var/www/autotech/autotech.ua/magmi/cli/magmi.cli.php -mode=create -profile=1c_import -f
 /usr/bin/php /var/www/autotech/autotech.ua/shell/indexer.php --reindexall -f
+echo "reindexall refreshed"
 /usr/bin/php /var/www/autotech/autotech.ua/shell/search_indexer.php indexation -f
+echo "indexation refreshed"
 /usr/bin/php -f /var/www/autotech/autotech.ua/tests/CleanCache.php
 echo "Cache FPC refreshed"
 #/usr/bin/php -f /var/www/autotech/autotech.ua/tests/XmlParser.php
